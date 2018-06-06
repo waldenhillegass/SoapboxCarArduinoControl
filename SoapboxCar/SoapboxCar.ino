626f86f858c19e3bc730b3d97073a0d957d61402
Walden Hillegass <waldenhillegass@gmail.com>
2018-06-06 16:43:37 -0400

    Added ramping with its own share of bugs

--- SoapboxCar/SoapboxCar.ino
+++ SoapboxCar/SoapboxCar.ino
@@ -1,10 +1,10 @@
 /*
- * Written by Walden Hillegass
- * June 5, 2018
- * 
- * This program takes potentiometer data and turns it into PWM signals which can be interpreted by a SPARK or TALON SR motorcontroller. 
- * NOT TESTED YET.
- */
+   Written by Walden Hillegass
+   June 5, 2018
+
+   This program takes potentiometer data and turns it into PWM signals which can be interpreted by a SPARK or TALON SR motorcontroller.
+   NOT TESTED YET.
+*/
 
 #include <Servo.h>
 
@@ -13,20 +13,25 @@
 //Pin for potentiometer
 #define POT_PIN 0
 
-//LED will be used to tell user they need to zero throttle
+//LED will be used to tell user they need to zero targetThrottle
 #define LED_PIN 13
 #define REV_BUTTON 7
 
 //Will test potentiometer in seperate tester program to get these values
-#define POT_MIN 0
-#define POT_MAX 1023
+double POT_MIN = 0;
+double POT_MAX = 1023;
 
+float maxThrottleChangePerSec = .02;
 
 //I have a feeling reverse mode might be finicky so here is a debug variable
-bool reversible = true;
+bool reversible = false;
 
 bool inReverse = false;
-float throttle;
+double targetThrottle = 0;
+double throttle = 0;
+
+double throttleLastCycle = 0;
+long timeLastCycle;
 
 //Hijacked the servo library to run motor controllers
 Servo Controller;
@@ -34,76 +39,114 @@ Servo Controller;
 void setup() {
   //Serial will be used for trouble shooting.
   Serial.begin(9600);
-  
+
   pinMode(REV_BUTTON, INPUT);
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW);
   Controller.attach(PWM_PIN);
   digitalWrite(PWM_PIN, LOW);
-
-//Throttle should be zeroed before PWM starts. #safety 
-  waitForZeroThrottle();
+  timeLastCycle = millis();
+  //targetThrottle should be zeroed before PWM starts. #safety
+  //waitForZeroThrottle();
 }
 
 
 void loop() {
   int PwmHighTime;
-  readThrottle();
- 
+  
+  readTargetThrottle();
+
+  //All of this is for ramping the throttle;
+  long timeSinceLastCycle = millis(); - timeLastCycle;
+  double maxThrottleChange = (timeSinceLastCycle / 10000) * maxThrottleChangePerSec;
+  if (targetThrottle > (throttleLastCycle + maxThrottleChange))
+  {
+    throttle = throttleLastCycle + maxThrottleChange;
+  } 
+  else if (targetThrottle < (throttleLastCycle - maxThrottleChange)) 
+  {
+    throttle = throttleLastCycle - maxThrottleChange;
+  }
+  else
+  {
+    throttle = targetThrottle;
+  }
+
+  timeLastCycle = millis();
+  throttleLastCycle = throttle;
+
+  
   Serial.println(throttle);
   
-  if(inReverse && reversible) {
-    
+  if (inReverse && reversible) {
+
     PwmHighTime = (int)map(throttle, 0, 1, 1500, 1000);
-    
+
     if (digitalRead(REV_BUTTON) == LOW)
     {
       if (isZeroThrottle) {
         digitalWrite(LED_PIN, LOW);
         inReverse = false;
-        //Must have throttle zeroed to switch direction. Doing otherwise will suck.
+        //Must have targetThrottle zeroed to switch direction. Doing otherwise will suck.
       } else {
         digitalWrite(LED_PIN, HIGH);
       }
-      
+
     }
   } else {
     //is not in reverse
-     PwmHighTime = (int) map(throttle, 0, 1, 1500, 2000);
+    PwmHighTime = (int) map(throttle, 0, 1, 1500, 2000);
 
-     if (digitalRead(REV_BUTTON) == HIGH)
-     {
+    if (digitalRead(REV_BUTTON) == HIGH)
+    {
       if (isZeroThrottle) {
         digitalWrite(LED_PIN, LOW);
         inReverse = false;
-        //Must have throttle zeroed to switch direction. Doing otherwise will suck.
+        //Must have targetThrottle zeroed to switch direction. Doing otherwise will suck.
       } else {
         digitalWrite(LED_PIN, HIGH);
       }
     }
 
   }
- Controller.writeMicroseconds(PwmHighTime);
-}
-
-float readThrottle(){
-  int potRead = analogRead(POT_PIN);
-  throttle = (float) map(potRead, POT_MIN, POT_MAX, 0, 1);
-  return throttle;
   
+  Controller.writeMicroseconds(PwmHighTime);
+  delay(20);
 }
 
-void waitForZeroThrottle(){
-  while(!isZeroThrottle())
-    {
-      digitalWrite(LED_PIN, HIGH);
+double readTargetThrottle() {
+  double potRead = analogRead(POT_PIN);
+  targetThrottle = doublemap(potRead, POT_MIN, POT_MAX, 0.00, 1.00);
+  if (targetThrottle < .04) {
+    targetThrottle = 0;
+  }
+  if (targetThrottle > 1) {
+    targetThrottle = 1;
+  }
+  
+  return targetThrottle;
+}
+
+void waitForZeroThrottle() {
+  while (readTargetThrottle() != 0)
+  {
+    Serial.println(readTargetThrottle());
+    digitalWrite(LED_PIN, HIGH);
     //do nothing.
-    }
+  }
   digitalWrite(LED_PIN, LOW);
 }
 
-bool isZeroThrottle(){
+bool isZeroThrottle() {
 
   return (throttle < .01);
 }
+
+
+double doublemap(double x, double in_min, double in_max, double out_min, double out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
 
